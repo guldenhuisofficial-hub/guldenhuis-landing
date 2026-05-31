5173f872aa9219ebf27babd7a7ccc5687b5ac8a1
Guldenhuis <guldenhuisofficial@gmail.com>
2026-05-31 14:02:21 +0700

    Add meta domain verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
@@ -6,6 +6,12 @@ import { cn } from '@/lib/utils';
 export const metadata: Metadata = {
   title: 'GuldenHuis',
   description: 'Premium Interior Design and Renovation Services',
+
+  verification: {
+    other: {
+      'facebook-domain-verification': 'urO6ihqonldqvnp5lkyu355ohelx7o',
+    },
+  },
 };
 
 export default function RootLayout({
